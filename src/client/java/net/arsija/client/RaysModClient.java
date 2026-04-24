1a2db6e545c8fab742a3a0cb1e26ec6e57db38b2
ArSiJa8 <silvan@arsija.net>
2026-04-24 21:59:10 +0200

    Initial commit

---
+++ src/client/java/net/arsija/client/RaysModClient.java
@@ -0,0 +1,10 @@
+package net.arsija.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RaysModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
